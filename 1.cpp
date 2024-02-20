ed4ecb845a5cb17385e6d1de76468a43ba8efbd6
jagnathreddy <98260819+JagnathReddy@users.noreply.github.com>
2024-02-20 16:08:18 +0530

    error

--- 1.cpp
+++ 1.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main() {
     //SysConf=> _SC_
-  cout << No. of clock ticks:  << sysconf(_SC_CLK_TCK) << endl;
-  cout << Max. no. of child processes:  << sysconf(_SC_CHILD_MAX)<< endl;
-  cout << Max. no. of open files/process:  << sysconf(_SC_OPEN_MAX)<< endl;
+  cout << "No. of clock ticks:"  << sysconf(_SC_CLK_TCK) << endl;
+  cout << "Max. no. of child processes:"  << sysconf(_SC_CHILD_MAX)<< endl;
+  cout << "Max. no. of open files/process:"  << sysconf(_SC_OPEN_MAX)<< endl;
   
     //PathConf=>_PC_ 
-  cout << Max. path length:  << pathconf(/, _PC_PATH_MAX) << endl;
-  cout << Max. no. of characters in a file name: << pathconf(/, _PC_NAME_MAX) << endl;
+  cout << "Max. path length:  "<< pathconf(/, _PC_PATH_MAX) << endl;
+  cout << "Max. no. of characters in a file name: "<< pathconf(/, _PC_NAME_MAX) << endl;
   return 0;
 }
