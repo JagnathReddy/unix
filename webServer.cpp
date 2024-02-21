119f21d187e59f4981e6d85f1acfbeeca3ada6c0
jagnath reddy <98260819+JagnathReddy@users.noreply.github.com>
2024-02-21 23:52:08 +0530

    Update webServer.cpp

--- webServer.cpp
+++ webServer.cpp
@@ -1,7 +1,4 @@
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
 #include <cstring>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -23,7 +20,7 @@ int main() {
         int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, &clientAddrSize);
         std::string response ="HTTP/1.1 200 OK\r\n";
         response += "Content-Type: text/html\r\n\r\n";
-        response += "<html><body><h1>SERVER RUNNING</h1><img src=\"https://i.pinimg.com/originals/b5/6a/61/b56a613813fffc2c82d75d3f0b45d3ea.jpg\" width=\"500\" height=\"600\"></img></body></html>\r\n";
+        response += "<html><body><h1>SERVER RUNNING</h1></body></html>\r\n";
         ssize_t bytesSent = send(clientSocket, response.c_str(), response.length(), 0);
         close(clientSocket);
     }
